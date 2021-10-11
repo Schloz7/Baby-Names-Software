dd89de09af773909c962e83dccd7badcb5ff401b
Carlos Schloz <40119096+Schloz7@users.noreply.github.com>
2021-10-11 17:54:42 +0100

    Add files via upload

---
+++ famousmale.py
@@ -0,0 +1,35 @@
+famous_males = {
+1: "Liam",
+2: "Noah",
+3: "Oliver",
+4: "Elijah",
+5: "William",
+6: "James",
+7: "Benjamin",
+8: "Lucas",
+9: "Henry",
+10: "Alexander",
+11: "Mason",
+12: "Michael",
+13: "Ethan",
+14: "Daniel",
+15: "Jacob"
+}
+
+famous_females = {
+1:"Olivia",
+2:"Emma",
+3:"Ava",
+4:"Charlotte",
+5:"Sophia",
+6:"Amelia",
+7:"Isabella",
+8:"Mia",
+9:"Evelyn",
+10:"Harper",
+11:"Camila",
+12:"Gianna",
+13:"Abigail",
+14:"Luna",
+15:"Ella"
+}
